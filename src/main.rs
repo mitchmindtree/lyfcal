7735664640fdcac016d7e06c5b33f4e6bf44fdef
mitchmindtree <mitchell.nordine@gmail.com>
2016-10-19 20:01:59 +1100

    Created a set_background fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    
+    set_background();
+}
+
+
+#[cfg(target_os="macos")]
+fn set_background() {
+    println!("We're on macos!!!!!");
+}
+
+#[cfg(target_os="windows")]
+fn set_background() {
+    println!("We're on windows... yuck!!!!!");
 }
